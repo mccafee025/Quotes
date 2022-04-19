e1467248fec270c25bb5e8e82bf8cc34f9ead170
mccafee025 <ademaamanka@gmail.com>
2022-04-19 22:57:51 +0300

    add author to array

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
       quotes:Quote[] = [
-            {id:1, name:'Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.'},
-            {id:2, name:'Be happy for this moment. This moment is your life.'},
-            {id:3, name:'The more man meditates upon good thoughts, the better will be his world and the world at large.'},
-            {id:4, name:'When it is obvious that goals can’t be reached, don’t adjust the goals, but adjust the action steps.'},
-            {id:5, name:'Happiness is the feeling that power increases — that resistance is being overcome.'},
-            {id:6, name:'The secret of happiness, you see is not found in seeking more, but in developing the capacity to enjoy less.'},
+            {id:1, name:'Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.', author:'Buddha'},
+            {id:2, name:'Be happy for this moment. This moment is your life.', author:'Omar Khayyam'},
+            {id:3, name:'The more man meditates upon good thoughts, the better will be his world and the world at large.', author:'Confucius'},
+            {id:4, name:'When it is obvious that goals can’t be reached, don’t adjust the goals, but adjust the action steps.', author:'Confucius'},
+            {id:5, name:'Happiness is the feeling that power increases — that resistance is being overcome.', author:'Friedrich Nietzsche'},
+            {id:6, name:'The secret of happiness, you see is not found in seeking more, but in developing the capacity to enjoy less.', author:'Socrates'},
       ];
   constructor() { }
 
