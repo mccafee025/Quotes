fd4f2cf911553a19bd43a49a426a0854152a765f
mccafee025 <ademaamanka@gmail.com>
2022-04-20 21:34:32 +0300

    add like and dislike

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
-import { QuoteDetailComponent } from './quote/quote-detail/quote-detail.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteDetailComponent
+    QuoteDetailComponent,
+    QuoteFormComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,16 +8,23 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
       quotes:Quote[] = [
-            {id:1, name:'Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.', author:'Buddha'},
-            {id:2, name:'Be happy for this moment. This moment is your life.', author:'Omar Khayyam'},
-            {id:3, name:'The more man meditates upon good thoughts, the better will be his world and the world at large.', author:'Confucius'},
-            {id:4, name:'When it is obvious that goals can’t be reached, don’t adjust the goals, but adjust the action steps.', author:'Confucius'},
-            {id:5, name:'Happiness is the feeling that power increases — that resistance is being overcome.', author:'Friedrich Nietzsche'},
-            {id:6, name:'The secret of happiness, you see is not found in seeking more, but in developing the capacity to enjoy less.', author:'Socrates'},
+            new Quote(1, 'Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.', 'Buddha', 1, 10),
+            new Quote(2, 'Be happy for this moment. This moment is your life.', 'Omar Khayyam', 2, 5),
+            new Quote(3, 'The more man meditates upon good thoughts, the better will be his world and the world at large.', 'Confucius', 4, 6),
+            new Quote(4, 'When it is obvious that goals can’t be reached, don’t adjust the goals, but adjust the action steps.', 'Confucius', 5, 3),
+            new Quote(5, 'Happiness is the feeling that power increases — that resistance is being overcome.', 'Friedrich Nietzsche', 5, 9),
+            new Quote(6, 'The secret of happiness, you see is not found in seeking more, but in developing the capacity to enjoy less.', 'Socrates',10, 8),
       ];
+      toggleDetails(index:any){
+            this.quotes[index].showDescription = !this.quotes[index].showDescription;
+      }
   constructor() { }
 
   ngOnInit(): void {
+  }  
+  showDesc(index:number) {
+       this.quotes[index].showDescription=!this.quotes[index].showDescription
+      
   }
 
 }
